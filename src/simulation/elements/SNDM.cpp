a23402cf5cb4dbf06b876fbf7efad0bf03ebb022
MK <marshall@apps.themarshallk.com>
2012-12-01 19:17:15 -0700

    Sound Element

---
+++ src/simulation/elements/SNDM.cpp
@@ -0,0 +1,109 @@
+#include "simulation/Elements.h"
+#include <conio.h>
+#include "inc/fmod.h"
+FSOUND_SAMPLE* handle;
+//#TPT-Directive ElementClass Element_SNDM PT_SNDM 167
+Element_SNDM::Element_SNDM()
+{
+    Identifier = "DEFAULT_PT_SNDM";
+    Name = "SNDM";
+    Colour = PIXPACK(0x40A060);
+    MenuVisible = 1;
+    MenuSection = SC_ELEC;
+    Enabled = 1;
+    
+    Advection = 0.0f;
+    AirDrag = 0.00f * CFDS;
+    AirLoss = 0.90f;
+    Loss = 0.00f;
+    Collision = 0.0f;
+    Gravity = 0.0f;
+    Diffusion = 0.00f;
+    HotAir = 0.000f	* CFDS;
+    Falldown = 0;
+    
+    Flammable = 0;
+    Explosive = 0;
+    Meltable = 0;
+    Hardness = 2;
+    
+    Weight = 100;
+    
+    Temperature = R_TEMP+0.0f	+273.15f;
+    HeatConduct = 0;
+    Description = "Sound Maker";
+    
+    State = ST_SOLID;
+    Properties = TYPE_SOLID;
+    
+    LowPressure = IPL;
+    LowPressureTransition = NT;
+    HighPressure = 15.0f;
+    HighPressureTransition = PT_BRMT;
+    LowTemperature = ITL;
+    LowTemperatureTransition = NT;
+    HighTemperature = ITH;
+    HighTemperatureTransition = NT;
+    
+    Update = &Element_SNDM::update;
+    Graphics = &Element_SNDM::graphics;
+}
+
+//#TPT-Directive ElementHeader Element_WIFI static int update(UPDATE_FUNC_ARGS)
+int Element_SNDM::update(UPDATE_FUNC_ARGS)
+ {
+	int r, rx, ry;
+	parts[i].tmp = (int)((parts[i].temp-73.15f)/100+1);
+	if (parts[i].tmp>=CHANNELS) parts[i].tmp = CHANNELS-1;
+	else if (parts[i].tmp<0) parts[i].tmp = 0;
+	for (rx=-1; rx<2; rx++)
+		for (ry=-1; ry<2; ry++)
+			if (x+rx>=0 && y+ry>0 && x+rx<XRES && y+ry<YRES && (rx || ry))
+			{
+				r = pmap[y+ry][x+rx];
+				if (!r)
+					continue;
+				// wireless[][0] - whether channel is active on this frame
+				// wireless[][1] - whether channel should be active on next frame
+				if (sim->wireless[parts[i].tmp][0])
+				{
+					if (((r&0xFF)==PT_NSCN||(r&0xFF)==PT_PSCN||(r&0xFF)==PT_INWR)&&parts[r>>8].life==0 && sim->wireless[parts[i].tmp][0])
+					{
+						//HI
+					}
+				}
+				else
+				{
+					if ((r&0xFF)==PT_SPRK && parts[r>>8].ctype!=PT_NSCN && parts[r>>8].life>=3)
+					{
+						  // init FMOD sound system
+							FSOUND_Init(44100, 32, 0);
+
+							// load and play mp3
+							handle=FSOUND_Sample_Load(0,"my.mp3",0, 0, 0);
+							FSOUND_PlaySound(0,handle);
+							 // wait until the users hits a key to end the app
+   
+						// clean up
+					}
+				}
+			}
+	return 0;
+}
+
+
+//#TPT-Directive ElementHeader Element_WIFI static int graphics(GRAPHICS_FUNC_ARGS)
+int Element_SNDM::graphics(GRAPHICS_FUNC_ARGS)
+
+{
+	float frequency = 0.0628;
+	int q = cpart->tmp;
+	*colr = sin(frequency*q + 0) * 127 + 128;
+	*colg = sin(frequency*q + 2) * 127 + 128;
+	*colb = sin(frequency*q + 4) * 127 + 128;
+	*pixel_mode |= EFFECT_DBGLINES;
+	return 0;
+}
+
+
+Element_SNDM::~Element_SNDM() {}
